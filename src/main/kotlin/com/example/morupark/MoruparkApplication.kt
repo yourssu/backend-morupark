7a199a343eeb56faa75e7d7c57293058d5f4487c
kim nan seul <154604350+seulnan@users.noreply.github.com>
2025-09-10 16:11:13 +0900

    [#1]; chore: 스프링 부트 초기세팅
    +) 깃허브 이슈&pr템플릿 추가

---
+++ src/main/kotlin/com/example/morupark/MoruparkApplication.kt
@@ -0,0 +1,11 @@
+package com.example.morupark
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MoruparkApplication
+
+fun main(args: Array<String>) {
+	runApplication<MoruparkApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/morupark/MoruparkApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.morupark
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MoruparkApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
