942e1cc79b2dfe0b4bc787b3c3d0210b5087a17a
dwl21 <nggus5@gmail.com>
2025-09-16 11:51:04 +0900

    [#3]; feat: JwtUtil 구현

---
+++ services-auth/src/main/kotlin/com/yourssu/morupark/auth/util/JwtUtil.kt
@@ -0,0 +1,46 @@
+package com.yourssu.morupark.auth.util
+
+import io.jsonwebtoken.JwtException
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys.hmacShaKeyFor
+import java.util.*
+
+
+class JwtUtil(
+    secret: String,
+    val expiration: Long
+) {
+    private val secretKey = hmacShaKeyFor(secret.toByteArray())
+
+    fun generateToken(userId: Long): String {
+        val token = Jwts.builder()
+            .subject(userId.toString())
+            .issuedAt(Date(System.currentTimeMillis()))
+            .expiration(Date(System.currentTimeMillis() + expiration))
+            .signWith(secretKey)
+            .compact()
+
+        return token
+    }
+
+    fun validateToken(token: String): Boolean {
+        try {
+            Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+        } catch (_: JwtException) {
+            return false
+        }
+        return true
+    }
+
+    fun getUserIdFromToken(token: String): Long {
+        return Jwts.parser()
+            .verifyWith(secretKey)
+            .build()
+            .parseSignedClaims(token)
+            .payload
+            .subject
+            .toLong()
+    }
+}
