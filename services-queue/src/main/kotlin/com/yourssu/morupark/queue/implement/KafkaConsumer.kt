4072a3ba7c8eb199651950e319ae2f9129d8b775
leeminseo <leeminseo0923@gmail.com>
2025-09-17 21:44:08 +0900

    [#9]; feat: Redistemplate 추가

--- services-queue/src/main/kotlin/com/yourssu/morupark/queue/implement/KafkaConsumer.kt
+++ services-queue/src/main/kotlin/com/yourssu/morupark/queue/implement/KafkaConsumer.kt
@@ -1,9 +1,24 @@
 package com.yourssu.morupark.queue.implement
 
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Component
 
 @Component
-class KafkaConsumer {
+class KafkaConsumer(
+    private val redisTemplate: RedisTemplate<String, >
+) {
 
+    companion object {
+        private const val TAG = "TOPIC"
+        private const val GROUP_ID = "ticketing_group"
+    }
+
+    @KafkaListener(topics = [TAG], groupId = GROUP_ID)
+    fun listen(
+        userId: Long
+    ) {
+
+    }
 
 }
