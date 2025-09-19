5d8931ce0c42eb286279969d77be6ae237a1178f
leeminseo <leeminseo0923@gmail.com>
2025-09-19 20:47:27 +0900

    [#9]; feat: webflux 추가

--- services-queue/build.gradle.kts
+++ services-queue/build.gradle.kts
@@ -8,4 +8,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-websocket")
 
     implementation("org.springframework.kafka:spring-kafka")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    testImplementation("io.projectreactor:reactor-test")
 }
