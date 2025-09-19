cf5f3211189030a8f5b4811247325df519306d9f
leeminseo <leeminseo0923@gmail.com>
2025-09-19 21:03:14 +0900

    [#9]; feat: accessToken 인증 로직 구현

--- services-queue/src/main/kotlin/com/yourssu/morupark/queue/implement/AuthAdapter.kt
+++ services-queue/src/main/kotlin/com/yourssu/morupark/queue/implement/AuthAdapter.kt
@@ -1,8 +1,26 @@
 package com.yourssu.morupark.queue.implement
 
 import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.WebClient
 
 @Component
 class AuthAdapter(
+    private val webClient : WebClient.Builder
 ) {
+    private val AUTH_SERVICE_URL = "http://localhost:8081"
+    private val webClientAuth = webClient.baseUrl(AUTH_SERVICE_URL).build()
+
+    fun isTokenValid(accessToken: String) : Boolean {
+        return webClientAuth.get()
+            .uri { uriBuilder ->
+                uriBuilder.path("/auth/token")
+                    .queryParam("accessToken", accessToken)
+                    .build()
+            }
+            .retrieve()
+            .bodyToMono(Boolean::class.java)
+            .block() ?: false
+    }
+
+
 }
