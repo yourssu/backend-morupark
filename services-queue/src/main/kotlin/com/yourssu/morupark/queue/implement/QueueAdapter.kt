e0331c111ebc6d7e71af03847351cbac8517716d
leeminseo <leeminseo0923@gmail.com>
2025-09-20 17:52:57 +0900

    [#9]; feat: 상태를 키로 구분하도록 변경

--- services-queue/src/main/kotlin/com/yourssu/morupark/queue/implement/QueueAdapter.kt
+++ services-queue/src/main/kotlin/com/yourssu/morupark/queue/implement/QueueAdapter.kt
@@ -6,17 +6,31 @@ import org.springframework.stereotype.Component
 
 @Component
 class QueueAdapter(
-    private val redisTemplate: RedisTemplate<String, UserInfo>
+    private val redisTemplate: RedisTemplate<String, String>
 ) {
 
-    private val QUEUE_KEY = "QUEUE"
+    private val QUEUE_WAITING_KEY = "queue:waiting"
+    private val QUEUE_ALLOWED_KEY = "queue:allowed"
 
     fun addToQueue(accessToken: String, timestamp: Long) {
-        redisTemplate.opsForZSet().add(QUEUE_KEY, UserInfo(accessToken, TicketStatus.WAITING), timestamp.toDouble())
+        redisTemplate.opsForZSet().add(QUEUE_WAITING_KEY, accessToken, timestamp.toDouble())
     }
 
     fun isInQueue(accessToken: String) : Boolean {
-        val score = redisTemplate.opsForZSet().score(QUEUE_KEY, UserInfo(accessToken, TicketStatus.WAITING))
+        val score = redisTemplate.opsForZSet().score(QUEUE_WAITING_KEY, accessToken)
+            ?: redisTemplate.opsForZSet().score(QUEUE_ALLOWED_KEY, accessToken)
         return score != null
+    }
+
+    /**
+     * 지금 내 상태가 ALLOWED인지 WAITING인지 조회
+     */
+    fun getTicketStatus(accessToken: String) : TicketStatus {
+        val rank = redisTemplate.opsForZSet().rank(QUEUE_WAITING_KEY, accessToken)
+        return if (rank != null) TicketStatus.WAITING else TicketStatus.ALLOWED
+    }
+
+    fun getRank(accessToken: String) : Long? {
+        return redisTemplate.opsForZSet().rank(QUEUE_WAITING_KEY, accessToken)
     }
 }
