0ea7c8ae205192367090f07ee1acf81317863bea
yueunfive <122276414+yueunfive@users.noreply.github.com>
2025-09-22 13:53:52 +0900

    fix: 스케줄링 기능 활성화

--- services-queue/src/main/kotlin/com/yourssu/morupark/queue/QueueApplication.kt
+++ services-queue/src/main/kotlin/com/yourssu/morupark/queue/QueueApplication.kt
@@ -2,7 +2,9 @@ package com.yourssu.morupark.queue
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableScheduling
 @SpringBootApplication
 class QueueApplication
 
