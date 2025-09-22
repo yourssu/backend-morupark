a9de58b8b695237b55bc2373ff67d62bb3efd736
yueunfive <122276414+yueunfive@users.noreply.github.com>
2025-09-22 13:49:23 +0900

    feat: 클라이언트의 대기상태 api 호출 결과가 ALLOWED일때 ALLOWED QUEUE에서 제거

--- services-queue/src/main/kotlin/com/yourssu/morupark/queue/application/QueueController.kt
+++ services-queue/src/main/kotlin/com/yourssu/morupark/queue/application/QueueController.kt
@@ -1,7 +1,6 @@
 package com.yourssu.morupark.queue.application
 
 import com.yourssu.morupark.queue.business.QueueService
-import org.apache.el.parser.Token
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
