7e65f09fd269e120d20e7a08287a7dbefc5489ac
dwl21 <nggus5@gmail.com>
2025-10-07 15:59:32 +0900

    [#26]; feat : 플랫폼 redirect-url 반환 API 구현

--- services-auth/src/main/kotlin/com/yourssu/morupark/auth/application/PlatformController.kt
+++ services-auth/src/main/kotlin/com/yourssu/morupark/auth/application/PlatformController.kt
@@ -1,17 +1,21 @@
 package com.yourssu.morupark.auth.application
 
+import com.yourssu.morupark.auth.annotation.UserId
 import com.yourssu.morupark.auth.application.dto.PlatformRegisterRequest
 import com.yourssu.morupark.auth.application.dto.PlatformRegisterResponse
 import com.yourssu.morupark.auth.business.PlatformService
+import com.yourssu.morupark.auth.business.dto.PlatformUrlResponse
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/external-servers")
+@RequestMapping("/platforms")
 class PlatformController(
     private val platformService: PlatformService,
 ) {
@@ -21,4 +25,10 @@ class PlatformController(
         return ResponseEntity.status(HttpStatus.CREATED)
             .body(response)
     }
+
+    @GetMapping("/url")
+    fun getPlatformUrl(@UserId userId: Long, @RequestHeader("X-Auth") adminKey: String): ResponseEntity<PlatformUrlResponse> {
+        return ResponseEntity.ok(platformService.getRedirectUrl(userId, adminKey))
+    }
+
 }
