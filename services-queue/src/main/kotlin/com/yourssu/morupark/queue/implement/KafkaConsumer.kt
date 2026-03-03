1f74812299a7ce81428ed79412faf9ea05be224a
yueunfive <122276414+yueunfive@users.noreply.github.com>
2026-03-03 12:42:02 +0900

    [#54]; chore: 미사용 코드 주석 처리

--- services-queue/src/main/kotlin/com/yourssu/morupark/queue/implement/KafkaConsumer.kt
+++ services-queue/src/main/kotlin/com/yourssu/morupark/queue/implement/KafkaConsumer.kt
@@ -1,31 +1,31 @@
-package com.yourssu.morupark.queue.implement
-
-import org.springframework.kafka.annotation.KafkaListener
-import org.springframework.kafka.support.KafkaHeaders
-import org.springframework.messaging.handler.annotation.Header
-import org.springframework.stereotype.Component
-
-@Component
-class KafkaConsumer(
-    private val queueAdapter: QueueAdapter,
-    private val authAdapter: AuthAdapter,
-) {
-
-    companion object {
-        private const val TAG = "WAITING"
-        private const val GROUP_ID = "ticketing_group"
-    }
-
-    @KafkaListener(topics = [TAG], groupId = GROUP_ID)
-    fun listen(
-        accessToken: String,
-        @Header(KafkaHeaders.TIMESTAMP) timestamp: Long,
-    ) {
-        val userInfo = authAdapter.getUserInfo(accessToken)
-        val platformId = userInfo.platform.platformId
-        val tps = userInfo.platform.tps
-        ServerTPSMap.put(platformId, tps)
-        queueAdapter.addToWaitingQueue(accessToken, timestamp, platformId)
-    }
-
-}
+//package com.yourssu.morupark.queue.implement
+//
+//import org.springframework.kafka.annotation.KafkaListener
+//import org.springframework.kafka.support.KafkaHeaders
+//import org.springframework.messaging.handler.annotation.Header
+//import org.springframework.stereotype.Component
+//
+//@Component
+//class KafkaConsumer(
+//    private val queueAdapter: QueueAdapter,
+//    private val authAdapter: AuthAdapter,
+//) {
+//
+//    companion object {
+//        private const val TAG = "WAITING"
+//        private const val GROUP_ID = "ticketing_group"
+//    }
+//
+//    @KafkaListener(topics = [TAG], groupId = GROUP_ID)
+//    fun listen(
+//        accessToken: String,
+//        @Header(KafkaHeaders.TIMESTAMP) timestamp: Long,
+//    ) {
+//        val userInfo = authAdapter.getUserInfo(accessToken)
+//        val platformId = userInfo.platform.platformId
+//        val tps = userInfo.platform.tps
+//        ServerTPSMap.put(platformId, tps)
+//        queueAdapter.addToWaitingQueue(accessToken, timestamp, platformId)
+//    }
+//
+//}
