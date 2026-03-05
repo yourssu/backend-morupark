b4acf37ccd80b573088dec4abe1ca292574fd658
anozanami <maguri02@naver.com>
2026-03-05 17:19:04 +0900

    [#51]; chore: k8s 내부 kafka 사용 위한 설정 수정

--- services-goods/build.gradle.kts
+++ services-goods/build.gradle.kts
@@ -6,6 +6,20 @@ plugins {
     kotlin("plugin.jpa") version "2.2.0"
     id("org.springframework.boot") version "3.4.1"
     id("io.spring.dependency-management") version "1.1.7"
+    id("com.google.cloud.tools.jib") version "3.4.4"
+}
+
+jib {
+    from {
+        image = "eclipse-temurin:21-jre"
+    }
+    to {
+        image = "asia-northeast3-docker.pkg.dev/yourssu-morupark/morupark-repo-private/goods-service:latest"
+    }
+    container {
+        mainClass = "com.yourssu.morupark.goods.GoodsApplicationKt"
+        ports = listOf("8083")
+    }
 }
 
 group = "com.yourssu.morupark"
@@ -19,16 +33,24 @@ java {
 
 repositories {
     mavenCentral()
+    google()
+    maven { url = uri("https://packages.confluent.io/maven/") }
 }
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.kafka:spring-kafka")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    // Kafka
+    implementation("org.springframework.kafka:spring-kafka")
+    // implementation("com.google.cloud.hosted.kafka:managed-kafka-auth-login-handler:1.0.5")
+    implementation("com.google.cloud:spring-cloud-gcp-starter:5.1.2")
+    implementation("org.apache.kafka:kafka-clients")
 
     runtimeOnly("com.mysql:mysql-connector-j")
 
