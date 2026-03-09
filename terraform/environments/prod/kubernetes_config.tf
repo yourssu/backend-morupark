90fe8e1ab5dc00697f5030a2913b85e35bbff77a
anozanami <maguri02@naver.com>
2026-03-10 08:38:01 +0900

    [#51]; chore: gcp 시크릿 리소스 External Secrets Operator 사용하도록 수정

--- terraform/environments/prod/kubernetes_config.tf
+++ terraform/environments/prod/kubernetes_config.tf
@@ -1,13 +1,14 @@
-# 이 파일의 리소스들은 terraform apply 후 GKE 클러스터가 생성된 다음에 동작합니다.
+# 쿠버네티스 리소스는 terraform apply 시
+# GKE 클러스터와 통신이 필요함.
 
-# Kubernetes Provider가 인증에 사용할 GKE 클러스터 정보 가져오기
+# Kubernetes Provider가 사용할 GKE 클러스터 정보 가져오기
 data "google_container_cluster" "gke_cluster" {
   name     = google_container_cluster.morupark_gke.name
   location = var.region
   project  = data.google_project.project.project_id
 }
 
-# 현재 gcloud 환경의 인증 정보를 가져오기 위한 데이터 소스
+# 현재 gcloud 인증 정보를 가져오기 위한 데이터 소스
 data "google_client_config" "default" {}
 
 provider "kubernetes" {
@@ -17,3 +18,28 @@ provider "kubernetes" {
     data.google_container_cluster.gke_cluster.master_auth[0].cluster_ca_certificate
   )
 }
+
+# morupark-gcp-secrets 리소스는 Terraform 관리에서 제외하고 
+# External Secrets Operator (ESO)를 통해 관리합니다.
+# (이전에 타임아웃 오류를 일으켰던 부분입니다.)
+
+/*
+resource "kubernetes_secret_v1" "morupark_gcp_secrets" {
+  metadata {
+    name      = "morupark-gcp-secrets"
+    namespace = var.namespace
+  }
+
+  data = {
+    # db.tf 와 secret_manager.tf에서 직접 참조
+    DB_PASSWORD    = data.google_secret_manager_secret_version.db_password_version.secret_data
+
+    # secret_manager.tf에서 참조 (Terraform으로 관리할 경우)
+    JWT_SECRET     = google_secret_manager_secret_version.jwt_secret_version.secret_data
+    DB_USERNAME    = google_secret_manager_secret_version.db_username_version.secret_data
+    REDIS_PASSWORD = google_secret_manager_secret_version.redis_password_version.secret_data
+  }
+
+  type = "Opaque"
+}
+*/
--- terraform/environments/prod/secret_manager.tf
+++ terraform/environments/prod/secret_manager.tf
@@ -5,6 +5,38 @@ resource "google_secret_manager_secret" "jwt_secret" {
   }
 }
 
+resource "google_secret_manager_secret_version" "jwt_secret_version" {
+  secret      = google_secret_manager_secret.jwt_secret.id
+  secret_data = "temporary-jwt-secret-replace-me-in-production"
+}
+
+# 기존에 존재하는 시크릿을 data 소스로 참조
+data "google_secret_manager_secret" "db_username_secret" {
+  secret_id = "morupark-db-username"
+}
+
+resource "google_secret_manager_secret_version" "db_username_version" {
+  secret      = data.google_secret_manager_secret.db_username_secret.id
+  secret_data = var.db_username
+}
+
+data "google_secret_manager_secret" "db_password_secret" {
+  secret_id = "morupark-db-password"
+}
+
+data "google_secret_manager_secret_version" "db_password_version" {
+  secret = data.google_secret_manager_secret.db_password_secret.id
+}
+
+data "google_secret_manager_secret" "redis_password_secret" {
+  secret_id = "morupark-redis-password"
+}
+
+resource "google_secret_manager_secret_version" "redis_password_version" {
+  secret      = data.google_secret_manager_secret.redis_password_secret.id
+  secret_data = var.db_password
+}
+
 resource "google_secret_manager_secret" "admin_key" {
   secret_id = "morupark-admin-key-prod"
   replication {
