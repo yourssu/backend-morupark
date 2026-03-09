186c580b9b629636dbd69d297092042bd23e8fbb
anozanami <maguri02@naver.com>
2026-03-10 08:39:35 +0900

    [#51]; chore: ingress IP 미할당 문제 해결

--- terraform/environments/prod/gke.tf
+++ terraform/environments/prod/gke.tf
@@ -1,5 +1,6 @@
 # GKE Autopilot 모드로 클러스터 생성 (운영 환경)
 resource "google_container_cluster" "morupark_gke" {
+  provider         = google-beta
   name             = "morupark-gke-prod"
   location         = var.region
   enable_autopilot = true
@@ -12,9 +13,25 @@ resource "google_container_cluster" "morupark_gke" {
   workload_identity_config {
     workload_pool = "${data.google_project.project.project_id}.svc.id.goog"
   }
+
+  addons_config {
+    http_load_balancing {
+      disabled = false
+    }
+  }
+
   release_channel {
     channel = "REGULAR"
   }
+
+  control_plane_endpoints_config {
+    dns_endpoint_config {
+      allow_external_traffic      = true
+      enable_k8s_certs_via_dns    = true
+      enable_k8s_tokens_via_dns   = true
+    }
+  }
+
   master_authorized_networks_config {
     dynamic "cidr_blocks" {
       for_each = toset(var.authorized_ip_cidrs)
--- terraform/environments/prod/ingress.tf
+++ terraform/environments/prod/ingress.tf
@@ -1,6 +1,9 @@
 resource "google_compute_global_address" "morupark_static_ip" {
-  name = "morupark-static-ip-prod"
+  name         = "morupark-static-ip-prod-v2" # ingress.yaml의 이름과 반드시 일치해야 함!
+  description  = "Static IP for Morupark Production Ingress"
+  address_type = "EXTERNAL"
 }
+
 
 resource "google_compute_managed_ssl_certificate" "morupark_cert" {
   name = "morupark-managed-cert-prod"
@@ -8,8 +11,4 @@ resource "google_compute_managed_ssl_certificate" "morupark_cert" {
   managed {
     domains = [var.domain]
   }
-}
-
-output "ingress_ip" {
-  value = google_compute_global_address.morupark_static_ip.address
 }
